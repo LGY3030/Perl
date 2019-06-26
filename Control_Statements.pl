5d276358d505de1d2c94fc9fd699f33635ff72f1
GuanYu Lin <lgy3030@gmail.com>
2019-06-26 18:27:56 +0800

    Update

--- Control_Statements.pl
+++ Control_Statements.pl
@@ -3,13 +3,13 @@
 print("\nConditional Control Statements:\n");
 my $name="friend";
 my $x=100;
-print ("HELLO!\n") if ($name eq "friend");
-$x-=10 if ($x == 100);
+print ("HELLO!\n") if ($name eq "friend"); #倒裝
+$x-=10 if ($x == 100); #倒裝
 print("$x");
 print("\n");
 
-my $if1=80;
-if($if1>=90){
+my $if1=80; 
+if($if1>=90){ # if-elsif-else
 	print("A");
 }
 elsif($if1>=80){
@@ -27,10 +27,51 @@ else{
 print("\n");
 
 my $if2=20;
-unless($if2==10){
+unless($if2==10){ # unless == if not
 	print("ok");
 }
 print("\n\n");
 
 # Loop Control Statements
 print("Conditional Control Statements:\n");
+
+for($i=0;$i<=10;$i++){ # for loop
+	print("$i ");
+}
+print("\n");
+
+my @forarray=(1,2,3,4,5,6);
+for($i=0;$i<=$#forarray;$i++){ #印出array
+	print("$forarray[$i] ");
+}
+print("\n");
+foreach $i (@forarray){ #印出array
+	print("$i ");
+}
+print("\n");
+
+for $i (1..10){ # 等於for loop
+	print("$i ");
+}
+print("\n");
+
+foreach $i (1..10){ # 等於for loop
+	print("$i ");
+}
+print("\n");
+
+my $whilei=10;
+while($whilei>=0){ #while
+	print("$whilei ");
+	$whilei--;
+}
+print("\n");
+
+$whilei=10;
+do{	#do-while
+	print("$whilei ");
+	$whilei--;
+}while($whilei>=0);
+print("\n");
+
+# break & continue == last & next
